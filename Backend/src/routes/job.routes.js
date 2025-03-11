c9a49b4a51c297a356608eb6f5cf36aa08dab686
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-03-11 09:28:58 +0530

    Update create-job-admin route to use createJobadmin function for improved job creation handling

--- Backend/src/routes/job.routes.js
+++ Backend/src/routes/job.routes.js
@@ -13,14 +13,11 @@ import { verifyJWT, isEmployee } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-
-
-
 router.route("/details").get(detailAboutJob); //Done
 router.route("/role").get(rolewiseJobs); //Done
 router.route("/searching-job").post(searchedJobs); //Done
 router.route("/create-job").post(verifyJWT, isEmployee, createJob); //Done
-router.route("/create-job-admin").post(createJob); //Done
+router.route("/create-job-admin").post(createJobadmin); //Done
 router.route("/delete-job").delete(verifyJWT, isEmployee, deleteJob); //Done
 router.route("/multi-delete").delete(verifyJWT, isEmployee, multipleDeleteJob); //Done
 router.route("/update-job").put(verifyJWT, isEmployee, updateJob); //Done
