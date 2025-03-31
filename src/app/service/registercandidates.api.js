d25cb030cb5866795ee8849ab8c88cf78eeb20b8
w3cdpass <killu1875@gmail.com>
2025-03-31 12:18:24 +0530

    No code changes detected; skipping commit.

--- src/app/service/registercandidates.api.js
+++ src/app/service/registercandidates.api.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+<<<<<<< Updated upstream
+=======
+//import { BASE_URL } from "./apis";
+>>>>>>> Stashed changes
 //const API_URL = "https://jobhub-project-official-1.onrender.com/api/candidates";
 const BASE_URL="https://jobhub-project.onrender.com/api/candidates"; //backup url
 
